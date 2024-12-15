27f91fde4fc3c06a36d0415482b9b3971d8c6a10
ecattrina <katy_060104@mail.ru>
2024-12-15 22:22:18 +0500

    Редактирование контроллера авторизации

---
+++ WebTeploobmenApp/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebTeploobmenApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(string login, string password)
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
